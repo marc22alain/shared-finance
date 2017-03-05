278d6733682807f57f2802f99f3767c17170b528
marc22alain <marc@marcotypes.ca>
2017-03-05 11:31:57 -0500

    Modifying access control headers

--- app.js
+++ app.js
@@ -39,7 +39,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cookieParser());
+
+
 app.use(express.static(path.join(__dirname, './build')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  next();
+});
 
 // default (?) route processor
 app.use('/', routes);
