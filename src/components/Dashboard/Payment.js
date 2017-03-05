c5d587cc406238db32ad9bf551a0a514c7dd90c0
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2017-03-05 00:13:15 -0500

    table added

--- src/components/Dashboard/Payment.js
+++ src/components/Dashboard/Payment.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import './../../styles/css/Payment.css';
 
 class Payment extends Component {
     constructor(){
         super();
         this.state = {
+            open:false,
             unpaidBill :[
                 {dueDate: "Feb 19", payee:"The Home Depot",amount:2000},
                 {dueDate: "Feb 19", payee:"The Home Depot",amount:2000}
             ]
         }
     }
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
   render() {
       let checking = 50000;
+      const style = {
+        margin: 12,
+    };
+    
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
     return (
     <div className="payment-container row"> 
         <div className="blue-bar col-lg-2">
@@ -34,38 +62,41 @@ class Payment extends Component {
                 <span>${checking}</span>
             </div>
         </div>
-        <div className="bills col-lg-4">
+        <div className="bills col-lg-6">
             <h3>Bills</h3>
             <div className="unpaid-bills">
+                <h4>Unpaid Bills</h4>
                 <Table>
-                    <TableHeader>
+                    <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
                     <TableRow>
-                        <TableHeaderColumn>ID</TableHeaderColumn>
-                        <TableHeaderColumn>Name</TableHeaderColumn>
+                        <TableHeaderColumn>Due Date</TableHeaderColumn>
+                        <TableHeaderColumn>Payee</TableHeaderColumn>
+                        <TableHeaderColumn>Amount</TableHeaderColumn>
                         <TableHeaderColumn>Status</TableHeaderColumn>
                     </TableRow>
                     </TableHeader>
                     <TableBody displayRowCheckbox={false}>
-                    <TableRow>
-                        <TableRowColumn>1</TableRowColumn>
-                        <TableRowColumn>John Smith</TableRowColumn>
-                        <TableRowColumn>Employed</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>2</TableRowColumn>
-                        <TableRowColumn>Randal White</TableRowColumn>
-                        <TableRowColumn>Unemployed</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>3</TableRowColumn>
-                        <TableRowColumn>Stephanie Sanders</TableRowColumn>
-                        <TableRowColumn>Employed</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>4</TableRowColumn>
-                        <TableRowColumn>Steve Brown</TableRowColumn>
-                        <TableRowColumn>Employed</TableRowColumn>
-                    </TableRow>
+                    { this.state.unpaidBill.map((data)=>{
+                        return (
+                            <TableRow>
+                                <TableRowColumn>{data.dueDate}</TableRowColumn>
+                                <TableRowColumn>{data.payee}</TableRowColumn>
+                                <TableRowColumn>${data.amount}</TableRowColumn>
+                                <TableRowColumn>
+                                    <RaisedButton label="Pay Now" onTouchTap={this.handleOpen} secondary={true} style={style} />
+                                    <Dialog
+                                        title="Dialog With Actions"
+                                        actions={actions}
+                                        modal={false}
+                                        open={this.state.open}
+                                        onRequestClose={this.handleClose}
+                                        >
+                                        The actions in this window were passed in as an array of React objects.
+                                    </Dialog>
+                                </TableRowColumn>
+                            </TableRow>
+                        )
+                    })}
                     </TableBody>
                 </Table>
             </div>
